e4e73b6c7433cf1d7c1898fbdb26f90de2b5aed6
kory tegman <korytegman@gmail.com>
2012-07-07 11:34:25 -0700

    remove an extra / in a url

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     
   }
   config.fog_directory  = 'korytegman'                     # required
-  config.fog_host       = 'https://korytegman.s3-website-us-east-1.amazonaws.com/'            # optional, defaults to nil
+  config.fog_host       = 'https://korytegman.s3-website-us-east-1.amazonaws.com'            # optional, defaults to nil
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
