f243992c522682c2f6724d2aaa684a7ddb2311ae
kory tegman <korytegman@gmail.com>
2013-02-12 17:50:22 -0800

    added infinite scroll to photo blog.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
   }
   config.fog_directory  = 'korytegman'                     # required
-  config.fog_host       = 'https://korytegman.s3.amazonaws.com'            # optional, defaults to nil
+  config.asset_host       = 'https://korytegman.s3.amazonaws.com'            # optional, defaults to nil
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
